e83765149d542fb90d9028d2671e0fa0f02b8559
hiro-mark <nhatquangpham@proton.me>
2026-01-23 17:01:49 +0700

    Continued code from last week

--- abcf/src/ComparatorExercise.java
+++ abcf/src/ComparatorExercise.java
@@ -1,20 +1,43 @@
-import java.util.ArrayList;
-
-class shopItems {
-    public String name;
-    public int price;
-
-    public shopItems(String n, int p) {
-        name = n;
-        price = p;
-    }
-}
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ComparatorExercise {
 
     public static void main(String[] args) {
-        ArrayList<shopItems> myShopItems = new ArrayList<shopItems>();
-        myShopItems.add(new shopItems("Apple iPhone 17 Pro Max at 2TB Storage", 2000));
-        System.out.println(myShopItems);
+        Map<String, Integer> shopItems = new HashMap<String, Integer>();
+        shopItems.put("iPhone 18 Pro Max base", 1799);
+        shopItems.put("DELL Alienware U96420G59", 4699);
+        shopItems.put("TV LG G6 OLED", 2499);
+        shopItems.put("Red Dragons playing cards", 2);
+        shopItems.put("Hershey's Chocolate", 4);
+        shopItems.put("Samsung S26 Ultra", 1399);
+        shopItems.put("Asus ROG Strix SCAR 17 (G733PY-XS96)", 1800);
+        shopItems.put("Odyssey G9 Monitor", 899);
+        shopItems.put("Bicycle Playing Cards Virts Moments edition", 13);
+        shopItems.put("Feastables Chocolate", 11);
+        shopItems.put("Feastables Chocolate", 11);
+        System.out.println("Welcome to shop");
+        Map <String, Integer> shopAlphabetical = shopItems.entrySet().stream().sorted(
+                Map.Entry.<String, Integer>comparingByValue())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (a, b) -> a,
+                        LinkedHashMap::new
+                ));
+        System.out.println("Items sorted by ascending value:");
+        shopAlphabetical.forEach((key, value) -> System.out.println("Item: " + key + ", price: $" + value));
+        Map <String, Integer> shopValue = shopItems.entrySet().stream().sorted(
+                        Map.Entry.<String, Integer>comparingByKey()).collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (a, b) -> a,
+                        LinkedHashMap::new
+                ));
+        System.out.println();
+        System.out.println("Items sorted by alphabetical order:");
+        shopValue.forEach((key, value) -> System.out.println("Item: " + key + ", price: $" + value));
     }
 }
